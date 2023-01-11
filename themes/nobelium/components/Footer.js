d4121c71911ad9420944617c80713c682c43c01f
lon8972 <119324121+lon8972@users.noreply.github.com>
2023-01-11 11:46:08 +0800

    Update Footer.js

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -1,6 +1,5 @@
 import BLOG from '@/blog.config'
 import DarkModeButton from '@/components/DarkModeButton'
-import Vercel from '@/components/Vercel'
 
 export const Footer = (props) => {
   const d = new Date()
